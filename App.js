77c581098d5dba346f401ac4b1443d1cdd0ca922
Jiyu <Jiyu@cjymdeiMac.local>
2018-06-04 10:29:18 +0800

    add rn-cli.config.js and modify .babelrc to support jsx and import alias

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomePage from 'containers/home';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <HomePage/>
+    );
+  }
+}
